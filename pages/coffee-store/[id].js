bce653ca500f82668d9d522cf98781dc9a9e5030
M-oses340 <mosesomwa7@gmail.com>
2023-02-11 08:58:40 +0300

    first commit

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,99 @@
+
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+
+import cls from "classnames";
+
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+import styles from "../../styles/coffee-store.module.css";
+
+export async function getStaticProps(context) {
+  const params = context.params;
+  console.log("params", params);
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id; //dynamic id
+      }),
+    },
+  };
+}
+export function getStaticPaths() {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+const CoffeeStore = (props) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
+
+  const handleUpvoteButton = () => {};
+
+  return (
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={imgUrl}
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={`Image of ${name} coffee store`}
+          />
+        </div>
+
+        <div className={styles.col2}>
+          <p>{address}</p>
+          <p>{neighbourhood}</p>
+          <div className={cls("glass", styles.col2)}>
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/places.svg" width="24" height="24" />
+              <p className={styles.text}>{address}</p>
+            </div>
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/nearMe.svg" width="24" height="24" />
+              <p className={styles.text}>{neighbourhood}</p>
+            </div>
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/star.svg" width="24" height="24" />
+              <p className={styles.text}>1</p>
+            </div>
+
+            <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+              Up vote!
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeStore;
---
+++ pages/world.js
@@ -0,0 +1,6 @@
+import styles from '../styles/World.module.css'
+
+const World = () => {
+    return <div className={styles.container}>Hello world</div>
+};
+export default World;

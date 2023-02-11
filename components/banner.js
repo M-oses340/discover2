70d6329038682af183403aec498be87d4602633e
M-oses340 <mosesomwa7@gmail.com>
2023-02-11 10:13:51 +0300

    code makeover

--- components/banner.js
+++ components/banner.js
@@ -1,20 +1,20 @@
-import styles from "../components/banner.module.css";
+import styles from "./banner.module.css";
 
 const Banner = (props) => {
-    return( 
-     <div className={styles.container}>
-        <h1 className={styles.title}>
-            <span className = {styles.title1}>Cofeee</span>
-            <span className = {styles.title2}>Connoisseur</span>
-        </h1>
-       <p className = {styles.subTitle}>Discover your local coffee shops!</p>
-       <div className = {styles.buttonWrapper}>
-        <button className = {styles.button}onClick={props.
-         handleOnClick}>
-            {props.buttonText}
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>Coffee</span>
+        <span className={styles.title2}>Connoisseur</span>
+      </h1>
+      <p className={styles.subTitle}>Discover your local coffee stores!</p>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} onClick={props.handleOnClick}>
+          {props.buttonText}
         </button>
-        </div>
-       </div>
-    );
+      </div>
+    </div>
+  );
 };
-export default Banner
+
+export default Banner;
--- pages/world.js
+++
@@ -1,6 +0,0 @@
-import styles from '../styles/World.module.css'
-
-const World = () => {
-    return <div className={styles.container}>Hello world</div>
-};
-export default World;
